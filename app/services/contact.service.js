79c528aa8fb1e7253d79a139ee59ae76605c3fec
Doan-Tran <trantuongdoan07@gmail.com>
2023-03-23 20:30:08 +0700

    Sua loi

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -34,12 +34,13 @@ class ContactService {
         const cursor = await this.Contact.find(filter);
         return await cursor.toArray();
         }
-        async findByName(name) {
+
+    async findByName(name) {
         return await this.find({
         name: { $regex: new RegExp(name), $options: "i" },
         });
     }
-
+    
     async findById(id) {
         return await this.Contact.findOne({
             _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
